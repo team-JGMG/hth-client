b8ebf5d8f81c45536e37cffdadfd7803d67ed001
Leechaeun1 <lce001105@gmail.com>
2025-07-13 18:26:14 +0900

    feat(style): setup-ui

---
+++ src/common/env.js
@@ -0,0 +1,2 @@
+export const SNAP_USER_INFO = import.meta.env.VITE_SNAP_USER_INFO
+export const SNAP_API_URL = import.meta.env.VITE_SNAP_API_URL
---
+++ src/components/layouts/BaseLayout.vue
@@ -0,0 +1,10 @@
+<template>
+  <main class="max-w-3xl min-h-screen p-0 relative bg-gray-100 mx-auto">
+    <CommonHeader />
+    <slot></slot>
+  </main>
+</template>
+
+<script setup>
+import CommonHeader from '@/components/layouts/CommonHeader.vue'
+</script>
