024ee8ff66810256eaa320d979f76f4856fc761c
chaeun <lce001105@gmail.com>
2025-07-18 13:49:24 +0900

    [FIX] #5 edit tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './public/**/*.html'],
   theme: {
     extend: {},
   },
