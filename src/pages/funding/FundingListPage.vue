52cff56bfe99edd99f66a225d474a25991068259
chaeun <lce001105@gmail.com>
2025-07-21 17:07:58 +0900

    [FEAT] #9 create mainpage ui

--- src/pages/funding/FundingListPage.vue
+++ src/pages/funding/FundingListPage.vue
@@ -1,3 +1,20 @@
 <!-- 매물 목록 페이지 -->
-<template>FundingListPage</template>
-<script setup></script>
+<!-- <template>FundingListPage</template>
+<script setup></script> -->
+
+<template>
+  <BaseLayout>
+    FundingDetailPage<PointSection />
+    <FundingListSection />
+
+    <PropertySection />
+    <TradeListSection
+  /></BaseLayout>
+</template>
+<script setup>
+import PointSection from '@/components/main/PointSection.vue'
+import FundingListSection from '@/components/main/FundingListSection.vue'
+import PropertySection from '@/components/main/PropertySection.vue'
+import TradeListSection from '@/components/main/TradeListSection.vue'
+import BaseLayout from '@/layouts/BaseLayout.vue'
+</script>
