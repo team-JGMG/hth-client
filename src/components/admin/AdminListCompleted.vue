a74e06073b869ac91786e54bad734638ebc5c6e5
minjeong <minjeongkim0212@gmail.com>
2025-07-22 17:42:11 +0900

    [FEAT] #17 create adminpage basic

---
+++ src/components/admin/AdminListCompleted.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="space-y-4">
+    <AdminListItemCard v-for="item in filtered" :key="item.id" v-bind="item" />
+  </div>
+</template>
+
+<script setup>
+import { computed, defineProps } from 'vue'
+import AdminListItemCard from './AdminListItemCard.vue'
+const props = defineProps({
+  list: Array,
+})
+
+const filtered = computed(() =>
+  props.list.filter((p) => ['승인됨', '거절됨', '만료됨'].includes(p.status)),
+)
+</script>
