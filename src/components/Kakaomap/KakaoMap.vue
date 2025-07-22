c2a1d5787b4f0ef53f14ddca0c77c9e02cf16314
Leechaeun1 <lce001105@gmail.com>
2025-07-22 22:17:59 +0900

    [FEAT] #16 add map func

--- src/components/Kakaomap/KakaoMap.vue
+++ src/components/Kakaomap/KakaoMap.vue
@@ -1,1 +1,21 @@
-<template>KakaoMap</template>
+<template>
+  <div class="w-full max-w-full overflow-hidden">
+    <KakaoMap
+      :lat="coordinate.lat"
+      :lng="coordinate.lng"
+      :draggable="true"
+      class="w-full h-[300px]"
+    >
+      <KakaoMapMarker :lat="coordinate.lat" :lng="coordinate.lng" />
+    </KakaoMap>
+  </div>
+</template>
+
+<script setup>
+import { KakaoMap, KakaoMapMarker } from 'vue3-kakao-maps'
+
+const coordinate = {
+  lat: 33.450701,
+  lng: 126.570667,
+}
+</script>
--- src/pages/funding/FundingDetailPage.vue
+++ src/pages/funding/FundingDetailPage.vue
@@ -1,3 +1,8 @@
 <!-- 매물 상세보기 페이지 -->
-<template>FundingDetailPage</template>
-<script setup></script>
+<template>
+  <DetailLayout> FundingDetailPage <KakaoMap /></DetailLayout>
+</template>
+<script setup>
+import KakaoMap from '@/components/Kakaomap/KakaoMap.vue'
+import DetailLayout from '@/layouts/DetailLayout.vue'
+</script>
