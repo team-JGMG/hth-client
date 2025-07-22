c1184d3ed371292da414166e91f37d90d2c45947
chaeun <lce001105@gmail.com>
2025-07-22 09:28:41 +0900

    [STYLE] #9 change layout

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,24 +1,19 @@
 <template>
-  <BlankLayout>
-    <DetailHeader>메롱</DetailHeader>
+  <BaseLayout>
     <PointSection />
     <FundingListSection />
     <TradeListSection />
     <PropertySection />
     <SaleCompleted />
     <CommonNav />
-  </BlankLayout>
+  </BaseLayout>
 </template>
-
 <script setup>
-// import BaseLayout from '@/layouts/BaseLayout.vue'
-// import DetailLayout from '@/layouts/DetailLayout.vue'
+import BaseLayout from '@/layouts/BaseLayout.vue'
 import SaleCompleted from '@/components/main/SaleCompleted/SaleCompleted.vue'
 import PropertySection from '@/components/main/PropertySection.vue'
 import FundingListSection from '@/components/main/FundingListSection.vue'
 import PointSection from '@/components/main/PointSection/PointSection.vue'
 import TradeListSection from '@/components/main/TradeListSection.vue'
-import BlankLayout from '@/layouts/BlankLayout.vue'
 import CommonNav from '@/layouts/CommonNav.vue'
-import DetailHeader from '@/layouts/DetailHeader.vue'
 </script>
