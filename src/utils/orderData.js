fb3b7a57811398b78c9e47e25d2870d7e2bfb0a5
chaeun <lce001105@gmail.com>
2025-07-25 10:37:05 +0900

    [FEAT] #30 add upper limit, lower limit

--- src/utils/orderData.js
+++ src/utils/orderData.js
@@ -1,6 +1,10 @@
 export const mockOrderbook = [
-  { price: '2,175', buyVolume: 96, sellVolume: 0 },
+  { price: '2,195', buyVolume: 72, sellVolume: 0 },
+  { price: '2,190', buyVolume: 38, sellVolume: 0 },
+  { price: '2,185', buyVolume: 58, sellVolume: 0 },
 
+  { price: '2,180', buyVolume: 58, sellVolume: 0 },
+  { price: '2,175', buyVolume: 96, sellVolume: 0 },
   { price: '2,170', buyVolume: 40, sellVolume: 0 },
   { price: '2,165', buyVolume: 8, sellVolume: 0 },
   { price: '2,160', buyVolume: 10, sellVolume: 0 },
@@ -9,6 +13,8 @@ export const mockOrderbook = [
   { price: '1,975', buyVolume: 0, sellVolume: 60 },
   { price: '1,980', buyVolume: 0, sellVolume: 20 },
   { price: '1,985', buyVolume: 0, sellVolume: 10 },
+  { price: '1,990', buyVolume: 0, sellVolume: 30 },
+  { price: '1,995', buyVolume: 0, sellVolume: 20 },
 ]
 
 // 현재 가격
