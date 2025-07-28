31a7bf54c73d1db48a4c46410814d56f0f42b77e
Leechaeun1 <lce001105@gmail.com>
2025-07-28 17:49:41 +0900

    [COMMENT] #48 remove annotation

--- src/components/trade/BuyAndSell/TradeForm.vue
+++ src/components/trade/BuyAndSell/TradeForm.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="p-4 bg-black text-white rounded-b-lg flex flex-col">
-    <!-- 금액 입력 -->
     <div class="mb-2">
       <div class="relative bg-white rounded-md px-3 py-2 flex items-center">
         <input
@@ -17,7 +16,6 @@
       </div>
     </div>
 
-    <!-- 수량 입력 -->
     <div class="mb-8">
       <div class="relative bg-white rounded-md px-3 py-2 flex items-center">
         <input
@@ -34,7 +32,6 @@
       </div>
     </div>
 
-    <!-- 버튼 -->
     <div class="mt-auto flex justify-center">
       <CompletedButton
         type="button"
@@ -49,7 +46,6 @@
       </CompletedButton>
     </div>
 
-    <!-- 확인 모달 -->
     <TradeConfirmModal
       :type="props.type"
       :amount="amount"
