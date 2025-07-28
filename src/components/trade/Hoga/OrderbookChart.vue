bf578a92414840cfda476734892a2a1252a72367
Leechaeun1 <lce001105@gmail.com>
2025-07-28 13:54:35 +0900

    [FIX] #27 change tag location

--- src/components/trade/Hoga/OrderbookChart.vue
+++ src/components/trade/Hoga/OrderbookChart.vue
@@ -1,3 +1,16 @@
+<template>
+  <div class="flex flex-col items-center gap-1 mb-10">
+    <div class="text-sm text-black font-bold">상한가</div>
+    <div class="text-base text-red-500 font-semibold">{{ maxPrice }} 원</div>
+
+    <div ref="scrollContainer" class="h-[410px] overflow-y-auto no-scrollbar w-full">
+      <AskingPriceComponent @centerIndex="handleCenterIndex" />
+    </div>
+
+    <div class="text-base text-blue-700 font-semibold">{{ minPrice }} 원</div>
+    <div class="text-sm text-black font-bold mt-0">하한가</div>
+  </div>
+</template>
 <script setup>
 import { ref, nextTick } from 'vue'
 import AskingPriceComponent from './AskingPriceComponent.vue'
@@ -20,20 +33,6 @@ const prices = mockOrderbook.map((item) => Number(item.price.replace(/,/g, '')))
 const maxPrice = Math.max(...prices).toLocaleString()
 const minPrice = Math.min(...prices).toLocaleString()
 </script>
-
-<template>
-  <div class="flex flex-col items-center gap-1 mb-10">
-    <div class="text-sm text-black font-bold">상한가</div>
-    <div class="text-base text-red-500 font-semibold">{{ maxPrice }} 원</div>
-
-    <div ref="scrollContainer" class="h-[410px] overflow-y-auto no-scrollbar w-full">
-      <AskingPriceComponent @centerIndex="handleCenterIndex" />
-    </div>
-
-    <div class="text-base text-blue-700 font-semibold">{{ minPrice }} 원</div>
-    <div class="text-sm text-black font-bold mt-0">하한가</div>
-  </div>
-</template>
 
 <style scoped>
 .no-scrollbar::-webkit-scrollbar {
