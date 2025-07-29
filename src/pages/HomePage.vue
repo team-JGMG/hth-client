17bc3fc8ce532d18a522d84aea84f2f41036926d
Leechaeun1 <lce001105@gmail.com>
2025-07-30 00:15:52 +0900

    [FEAT] #52 add pinia

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,19 +1,30 @@
-<template>
-  <BaseLayout>
-    <PointSection />
-    <div class="flex space-x-3">
-      <FundingListSection class="flex-1" /> <TradeListSection class="flex-1" />
-    </div>
-    <PropertySection />
-    <SaleCompleted />
-  </BaseLayout>
-</template>
-
 <script setup>
+import { useOnboardingStore } from '@/stores/onboarding'
+import { storeToRefs } from 'pinia'
+
 import BaseLayout from '@/layouts/BaseLayout.vue'
 import SaleCompleted from '@/components/main/SaleCompleted/SaleCompleted.vue'
 import PropertySection from '@/components/main/PropertySection.vue'
 import FundingListSection from '@/components/main/FundingListSection.vue'
 import PointSection from '@/components/main/PointSection/PointSection.vue'
 import TradeListSection from '@/components/main/TradeListSection.vue'
+import OnboardingPage from './onboarding/OnboardingPage.vue'
+
+const onboardingStore = useOnboardingStore()
+const { hasVisited } = storeToRefs(onboardingStore)
 </script>
+
+<template>
+  <div>
+    <OnboardingPage v-if="!hasVisited" />
+    <BaseLayout v-else>
+      <PointSection />
+      <div class="flex space-x-3">
+        <FundingListSection class="flex-1" />
+        <TradeListSection class="flex-1" />
+      </div>
+      <PropertySection />
+      <SaleCompleted />
+    </BaseLayout>
+  </div>
+</template>
