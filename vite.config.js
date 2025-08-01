b8a2ab24f588d571e7f0de63c5f019d900e2c861
chaeun <lce001105@gmail.com>
2025-08-01 13:31:28 +0900

    [FIX] #74 edit vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  define: {
+    global: 'window',
+  },
   // server: {
   //   proxy: {
   //     '/api': {
