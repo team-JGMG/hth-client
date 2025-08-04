b07e543d391bd11453a86fb417df1344f7a012dd
sanghyunbbb <parksanghyun9999@gmail.com>
2025-08-04 14:31:50 +0900

    [FEAT] #81 make connection API of My SoldList

--- src/api/property.js
+++ src/api/property.js
@@ -1,1 +1,28 @@
 // 매물 api(등록, 조회, 삭제)
+
+import api from '@/libs/axios' // axios 인스턴스를 가져옴
+
+export const fetchUserProperties = async (userId) => {
+  try {
+    const statuses = ['PENDING', 'approved', 'rejected', 'sold']
+    const allResults = await Promise.all(
+      statuses.map((status) =>
+        api.get(`/api/property/user/${userId}`, {
+          params: {
+            page: 0,
+            size: 10,
+            status,
+          },
+        }),
+      ),
+    )
+
+    // 응답에서 각각의 데이터(content)만 추출하여 하나의 배열로 합치기
+    const merged = allResults.flatMap((res) => res.data?.data?.content || [])
+
+    return merged
+  } catch (error) {
+    console.error('매물 조회 실패:', error)
+    throw error
+  }
+}
