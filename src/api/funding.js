1cf5b6cc580d5d51fb8bd90078e86c9afa99e375
minjeong <minjeongkim0212@gmail.com>
2025-08-04 10:12:56 +0900

    [FIX] #87  fix axios and remove proxy

--- src/api/funding.js
+++ src/api/funding.js
@@ -1,34 +1,4 @@
-// //임시 목데이터 사용
-
-// import { mockFundingList } from '@/mocks/fundingListMock'
-
-// export const getFundingList = async (category, sort = 'date') => {
-//   let list = [...mockFundingList]
-
-//   // 1. 필터: category
-//   const statusMap = {
-//     funding: '모집 중',
-//     ended: '펀딩 완료',
-//     sold: '매각 완료',
-//   }
-//   if (category && statusMap[category]) {
-//     list = list.filter((item) => item.status === statusMap[category])
-//   }
-
-//   // 2. 정렬: sort
-//   if (sort === 'timeLeft') {
-//     list.sort((a, b) => a.daysLeft - b.daysLeft)
-//   } else if (sort === 'rate') {
-//     list.sort((a, b) => b.fundingRate - a.fundingRate)
-//   } else {
-//     list.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
-//   }
-
-//   return Promise.resolve({ data: list })
-// }
-// src/api/funding.js
-import axios from '@/libs/axios' // ← 프로젝트에 설정된 axios 인스턴스 사용
-
+import axios from '@/libs/axios'
 /**
  * 펀딩 목록 API 호출
  * @param {string} category - funding | ended | sold
