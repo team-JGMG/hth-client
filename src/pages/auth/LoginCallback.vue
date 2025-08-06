dade7d0fdb27ef4482dae827cc2b4a17444f8eaf
SANGHYUNBBB <parksanghyun9999@gmail.com>
2025-08-06 17:53:03 +0900

    [FEAT] #113  Redirect to admin page if user is admin.

--- src/pages/auth/LoginCallback.vue
+++ src/pages/auth/LoginCallback.vue
@@ -12,7 +12,6 @@ const authStore = useAuthStore()
 // ✅ status, email은 query에서 가져옴
 const status = route.query.status
 const email = route.query.email
-
 const handleCallback = async () => {
   console.log('[소셜 로그인 콜백 시작]')
   console.log('status:', status)
@@ -20,13 +19,18 @@ const handleCallback = async () => {
 
   if (status === 'SUCCESS') {
     try {
-      const res = await fetchUserInfo() // 서버가 쿠키로 유저 인증
+      const res = await fetchUserInfo() // ✅ 서버가 쿠키로 유저 인증
       console.log('유저 정보:', res.data)
 
       authStore.setUserInfo(res.data)
       authStore.setLoggedIn(true)
 
-      router.push('/')
+      // ✅ 유저 status에 따라 라우팅 분기
+      if (res.data.status === 'admin') {
+        router.push('/admin/property-review')
+      } else {
+        router.push('/')
+      }
     } catch (err) {
       console.error('[유저 정보 가져오기 실패]', err)
       router.push('/auth/login?error=user_fetch_failed')
