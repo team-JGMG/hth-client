102ee99b676df72cfb0532e75eba3ff6db7e63ca
chaeun <lce001105@gmail.com>
2025-08-07 23:27:15 +0900

    [FEAT] connected tradelistpage api

--- src/pages/trade/TradeListPage.vue
+++ src/pages/trade/TradeListPage.vue
@@ -1,13 +1,26 @@
 <template>
   <BlankLayout>
     <DetailHeader>거래중인 건물</DetailHeader>
-    <TradingListCard :items="mockTradeListData" />
+    <TradingListCard :items="tradeItems" />
   </BlankLayout>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
 import TradingListCard from '@/components/trade/TradingListCard.vue'
 import BlankLayout from '@/layouts/BlankLayout.vue'
 import DetailHeader from '@/layouts/DetailHeader.vue'
-import { mockTradeListData } from '@/utils/mockTradeListData.js'
+import { getEndedFundings } from '@/api/funding'
+
+const tradeItems = ref([])
+
+onMounted(async () => {
+  try {
+    const res = await getEndedFundings()
+    console.log('[응답 확인]', res.data)
+    tradeItems.value = res.data?.data?.content || []
+  } catch (error) {
+    console.error('펀딩 목록 불러오기 실패:', error)
+  }
+})
 </script>
