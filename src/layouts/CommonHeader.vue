4cae96340816e76396ba974781a8230363edf9a9
chaeun <lce001105@gmail.com>
2025-08-11 15:14:49 +0900

    [FEAT] #111 add animation

--- src/layouts/CommonHeader.vue
+++ src/layouts/CommonHeader.vue
@@ -4,12 +4,14 @@
   >
     <BaseLogo />
 
-    <img
-      src="@/assets/images/alert/alert.png"
-      alt="알림"
-      class="w-8 h-8 object-contain cursor-pointer"
-      @click="goFcmTest"
-    />
+    <div class="relative cursor-pointer" @click="goFcmTest">
+      <img src="@/assets/images/alert/alert.png" alt="알림" class="w-8 h-8 object-contain" />
+      <!-- 빨간 점 (통통 튀는 애니메이션, 더 붙임) -->
+      <span
+        v-if="nStore.unreadCount > 0"
+        class="absolute -top-0.5 -right-0.5 w-2 h-2 bg-red-500 rounded-full border border-white animate-bounce"
+      ></span>
+    </div>
   </header>
   <div class="h-20"></div>
 </template>
@@ -17,9 +19,12 @@
 <script setup>
 import BaseLogo from '@/components/common/Logo/BaseLogo.vue'
 import { useRouter } from 'vue-router'
+import { useNotificationStore } from '@/stores/notification'
 
 const router = useRouter()
+const nStore = useNotificationStore()
+
 const goFcmTest = () => {
-  router.push('/fcm-test')
+  router.push('/fcm-test') // 또는 '/notifications'
 }
 </script>
