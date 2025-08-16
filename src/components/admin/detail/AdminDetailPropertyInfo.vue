68ef5ec9490ec5a8afa004ee7742fcf18a688f2c
minjeong <minjeongkim0212@gmail.com>
2025-08-16 18:57:17 +0900

    [FIX] #278 componentization AdminDetailPage

---
+++ src/components/admin/detail/AdminDetailPropertyInfo.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="mt-4 px-2 bg-white">
+    <BaseTypography class="mb-2" weight="semibold" size="lg">매물 정보</BaseTypography>
+    <div class="px-1 pb-2">
+      <div class="grid grid-cols-[115px,1fr] text-sm border rounded-md overflow-hidden">
+        <BaseTypography class="bg-gray-50 px-3 py-2" weight="regular" color="gray-1">매물명</BaseTypography>
+        <BaseTypography weight="regular" class="px-3 py-2">{{ property.title }}</BaseTypography>
+        <BaseTypography class="bg-gray-50 px-3 py-2" weight="regular" color="gray-1">매물 주소</BaseTypography>
+        <BaseTypography weight="regular" class="px-3 py-2">{{ property.address }}</BaseTypography>
+        <BaseTypography class="bg-gray-50 px-3 py-2" weight="regular" color="gray-1">매물 면적</BaseTypography>
+        <BaseTypography weight="regular" class="px-3 py-2">{{ property.area }}㎡({{ formatAreaToPyeong(property.area) }})</BaseTypography>
+        <BaseTypography class="bg-gray-50 px-3 py-2" weight="regular" color="gray-1">희망 매매가</BaseTypography>
+        <BaseTypography weight="regular" class="px-3 py-2">{{ formatPriceInManwon(property.price) }}</BaseTypography>
+        <BaseTypography class="bg-gray-50 px-3 py-2" weight="regular" color="gray-1">희망 공모 기간</BaseTypography>
+        <BaseTypography weight="regular" class="px-3 py-2">{{ property.postingPeriod }}개월</BaseTypography>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import BaseTypography from '@/components/common/Typography/BaseTypography.vue'
+import { formatPriceInManwon, formatAreaToPyeong } from '@/utils/format'
+
+defineProps({
+  property: Object,
+})
+</script>
---
+++ src/components/admin/detail/AdminDetailSellerInfo.vue
@@ -0,0 +1,24 @@
+<template>
+  <section v-if="seller" class="mt-1 px-2 bg-white">
+    <BaseTypography class="mb-2" weight="semibold" size="lg">매도자 정보</BaseTypography>
+    <div class="px-1 pb-2">
+      <div class="grid grid-cols-[115px,1fr] text-sm border rounded-md overflow-hidden">
+        <BaseTypography class="bg-gray-50 px-3 py-2" weight="regular" color="gray-1">이름</BaseTypography>
+        <BaseTypography weight="regular" class="px-3 py-2">{{ seller.name || '-' }}</BaseTypography>
+        <BaseTypography class="bg-gray-50 px-3 py-2" weight="regular" color="gray-1">전화번호</BaseTypography>
+        <BaseTypography weight="regular" class="px-3 py-2">{{ formatPhoneNumber(seller.phone || '-') }}</BaseTypography>
+        <BaseTypography class="bg-gray-50 px-3 py-2" weight="regular" color="gray-1">이메일</BaseTypography>
+        <BaseTypography weight="regular" class="px-3 py-2">{{ seller.email || '-' }}</BaseTypography>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import BaseTypography from '@/components/common/Typography/BaseTypography.vue'
+import { formatPhoneNumber } from '@/utils/format'
+
+defineProps({
+  seller: Object,
+})
+</script>
