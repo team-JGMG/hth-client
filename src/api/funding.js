30b0f39f95d0ba125b52bb1b5d691cffb7001e5e
seul <angel3030303@gmail.com>
2025-08-18 01:23:16 +0900

    [REFACTOR] Modify API end point

--- src/api/funding.js
+++ src/api/funding.js
@@ -5,7 +5,7 @@ import api from '@/libs/axios'
 
 // 펀딩 목록
 export const getFundingList = async (category, sort = 'date', page = 0, size = 20) => {
-  return api.get('/api/funding', {
+  return api.get('/api/fundings', {
     params: {
       category,
       sort,
@@ -17,7 +17,7 @@ export const getFundingList = async (category, sort = 'date', page = 0, size = 2
 
 // 특정유저 펀딩 주문 목록 조회
 export const getUserFundingOrders = (status = 'pending', page = 0, size = 10) => {
-  return api.get(`/api/auth/funding-order`, {
+  return api.get(`/api/auth/funding-orders`, {
     params: {
       status,
       page,
@@ -47,7 +47,7 @@ export const refundFundingOrder = (fundingId, orderId, orderPrice) => {
     console.log('[refundFundingOrder] params =', params)
   }
 
-  return api.post('/api/auth/funding-order/refund', null, {
+  return api.post('/api/auth/funding-orders/refund', null, {
     params,
     withCredentials: true,
   })
@@ -60,14 +60,14 @@ export const getAllocations = (fundingId) => {
 
 // 거래 리스트 페이지
 export const getEndedFundings = async (page = 0, size = 10) => {
-  return api.get('/api/funding/ended', {
+  return api.get('/api/fundings/ended', {
     params: { page, size },
   })
 }
 
 // 거래 상세 페이지
 export const getFundingById = async (id) => {
-  const res = await api.get(`/api/funding/${id}`)
+  const res = await api.get(`/api/fundings/${id}`)
   if (import.meta.env.DEV) console.log('[API] getFundingById raw', id, res.status, res.data)
   return res
 }
